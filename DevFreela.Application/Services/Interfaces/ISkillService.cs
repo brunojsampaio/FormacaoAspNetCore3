54e3c20afc9f15fd121dc5237934b646092ee285
Bruno Sampaio <brunojsampaio@gmail.com>
2021-05-06 18:14:53 -0300

    feat: Created SkillService

---
+++ DevFreela.Application/Services/Interfaces/ISkillService.cs
@@ -0,0 +1,11 @@
+using System.Collections.Generic;
+using DevFreela.Application.ViewModels;
+
+namespace DevFreela.Application.Services.Interfaces
+{
+    public interface ISkillService
+    {
+        List<SkillViewModel> GetAll();
+        
+    }
+}
