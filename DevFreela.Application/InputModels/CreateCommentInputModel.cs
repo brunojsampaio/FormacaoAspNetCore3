9c5def47bdcd8eb6a422b6240bf6ac9d6e60f23d
Bruno Sampaio <brunojsampaio@gmail.com>
2021-05-06 10:43:58 -0300

    feat: Implemented ProjectService

--- DevFreela.Application/InputModels/CreateCommentInputModel.cs
+++ DevFreela.Application/InputModels/CreateCommentInputModel.cs
@@ -3,5 +3,8 @@ namespace DevFreela.Application.InputModels
     public class CreateCommentInputModel
     {
         
+        public string Content { get; set; }
+        public int IdProject { get; set; }
+        public int IdUser { get; set; }
     }
 }
