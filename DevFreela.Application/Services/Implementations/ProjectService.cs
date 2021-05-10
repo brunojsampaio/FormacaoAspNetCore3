47cc2bfc293238e200a32d978c78919e0ccfbaa6
Bruno Sampaio <brunojsampaio@gmail.com>
2021-05-10 18:39:52 -0300

    refactor: Changed Start Project and GetAll Skills to use Dapper

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -1,21 +1,26 @@
 using System.Collections.Generic;
 using System.Linq;
+using Dapper;
 using DevFreela.Application.InputModels;
 using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
 
 namespace DevFreela.Application.Services.Implementations
 {
     public class ProjectService : IProjectService
     {
         private readonly DevFreelaDbContext _dbContext;
+        private readonly string _connectionString;
 
-        public ProjectService(DevFreelaDbContext dbContext)
+        public ProjectService(DevFreelaDbContext dbContext, IConfiguration configuration)
         {
             _dbContext = dbContext;
+            _connectionString = configuration.GetConnectionString("DevFreelaCs");
         }
 
         public List<ProjectViewModel> GetAll(string query)
@@ -91,7 +96,16 @@ namespace DevFreela.Application.Services.Implementations
             var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
             
             project.Start();
-            _dbContext.SaveChanges();
+            // _dbContext.SaveChanges();
+
+            using (var sqlConnection = new SqlConnection(_connectionString))
+            {
+                sqlConnection.Open();
+
+                var script = "UPDATE project SET Status = @status, StartedAt = @startedat WHERE Id = @id";
+                
+                sqlConnection.Execute(script, new { status = project.Status, startedat = project.StartedAt, id });
+            }
         }
 
         public void Finish(int id)
