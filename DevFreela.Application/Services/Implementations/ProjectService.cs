0dcd110ea0a22c05bd11d0fde03abdcfba98a70b
Bruno Sampaio <brunojsampaio@gmail.com>
2021-05-10 17:52:12 -0300

    refactor: Included FullName of client and freelancer in ProjectDetailsViewModel

--- DevFreela.Application/Services/Implementations/ProjectService.cs
+++ DevFreela.Application/Services/Implementations/ProjectService.cs
@@ -5,6 +5,7 @@ using DevFreela.Application.Services.Interfaces;
 using DevFreela.Application.ViewModels;
 using DevFreela.Core.Entities;
 using DevFreela.Infrastructure.Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace DevFreela.Application.Services.Implementations
 {
@@ -30,7 +31,10 @@ namespace DevFreela.Application.Services.Implementations
 
         public ProjectDetailsViewModel GetById(int id)
         {
-            var project = _dbContext.Projects.SingleOrDefault(p => p.Id == id);
+            var project = _dbContext.Projects
+                .Include(p => p.Client)
+                .Include(p => p.Freelancer)
+                .SingleOrDefault(p => p.Id == id);
 
             return project == null ? null : new ProjectDetailsViewModel(
                 project.Id,
@@ -38,7 +42,9 @@ namespace DevFreela.Application.Services.Implementations
                 project.Description,
                 project.TotalCost,
                 project.StartedAt,
-                project.FinishedAt);
+                project.FinishedAt,
+                project.Client.FullName,
+                project.Freelancer.FullName);
         }
 
         public int Create(NewProjectInputModel inputModel)
